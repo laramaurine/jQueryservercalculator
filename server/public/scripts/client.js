feaf158ce674fbc9e25acd5c3d0bb8ccea039cfd
Lara Lesar <lara.m.lesar@gmail.com>
2020-11-13 16:40:57 -0600

    clear inputs function added and working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,19 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('jquery here for the party');
+    $('#clear').on('click', clearInputs)
+    //
     
+}
+
+//variable to send data to server
+//variable to store data to splash onto dom
+//function to post data
+//function to clear inputs
+//function to render data
+
+function clearInputs(){
+    console.log('inputs cleared');
+    $('#firstInput').val('');
+    $('#secondInput').val('');
 }
--- server/server.js
+++ server/server.js
@@ -9,6 +9,12 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+mathing = [];//completed maths will be stored here to splash onto dom
+//store problems completed
+//app.get 
+//app.post with the logic (mathing of numbers, can i do this in one function
+// or do i need seperate functions for each operator)
+
 // GET & POST Routes go here
 
 
