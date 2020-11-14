0a0c68f3fb72ff55c00c25a6eb809d6f829300d1
Lara Lesar <lara.m.lesar@gmail.com>
2020-11-14 14:26:51 -0600

    start of post msg to server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,13 +9,24 @@ function onReady(){
     
 }
 
-//variable to send data to server
+//variable to send data(numbers) to server .data
 //variable to store data to splash onto dom
 //function to post data
-//function to clear inputs
+
 //function to render data
+function handleNumbers(){
+    let numbers = {
+        num1: $('#firstInput').val(),
+        operator: $('#perator').val(),
+        num2: $('#secondInput').val()
+    }
+    //this packages up the info from the dom
+    //getting it ready to send
+    console.log('handling the numbers');
+}
 
 function clearInputs(){
+    //function to clear inputs -- done
     console.log('inputs cleared');
     $('#firstInput').val('');
     $('#secondInput').val('');
