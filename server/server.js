1b42c5c2253e13bdb481527deb0ab27cea31de9b
Lara Lesar <lara.m.lesar@gmail.com>
2020-11-15 15:03:00 -0600

    cleaned up comments

--- server/server.js
+++ server/server.js
@@ -67,12 +67,3 @@ app.listen(PORT, () => {
 })
 
 
-// You're really close! You're able to make calculations inn the server and able to append them to the DOM! YES!
-
-// When I refresh the page, the history of your calculations gets wiped away. You've got the work done on the server to have the history, but we need a GET request for that history on page load. This is related to the next point:
-
-// It looks like you're using a response from a POST for your append, Usually, the pattern is POST (send 201) -> GET (send DATA) -> append.
-
-// So the missing piece is from your client, you need to have a GET request function, and call it on load of page to populate the history (similar append logic that you've already written), and to call it after the POST is done.
-
-// See if you can get this working when you refresh, use Kris's livesolve for help.
